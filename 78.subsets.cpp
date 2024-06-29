ee7926669aa6acbc3719e4b2668070d6aa9fc4d5
ALOK MADDHESHIYA <96016294+alok8756@users.noreply.github.com>
2024-06-29 18:20:04 +0530

    Add files via upload

---
+++ 78.subsets.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    
+    vector<vector<int>> allSubsets;
+    void generate(vector<int> & subset,int i,vector<int> & nums){
+        if(i==nums.size()){
+            allSubsets.push_back(subset);
+            return ;
+        }
+        //ith element not in subset
+        generate(subset,i+1,nums);
+
+        // ith element in subsets
+        subset.push_back(nums[i]);
+        generate(subset,i+1,nums);
+        subset.pop_back();
+    }
+    
+    
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int>subset;
+        generate(subset,0,nums);
+        return allSubsets;
+    }
+};
